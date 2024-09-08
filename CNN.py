ac26130428ec97c910b1431c334216c4d78cdef9
shou-du <769921010@qq.com>
2024-09-08 14:22:34 +0800

    add ODconv.py

--- CNN.py
+++ CNN.py
@@ -3,6 +3,9 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn import init
 import math
+from odconv import ODConv2d
+
+
 class depthwise_separable_conv(nn.Module):
     def __init__(self, ch_in, ch_out):
         super(depthwise_separable_conv, self).__init__()
@@ -31,6 +34,7 @@ class depthwise_separable_1Dconv(nn.Module):
         x = self.point_conv(x)
         # print(x.shape)
         return x
+
 
 class SKConv(nn.Module):
     def __init__(self, inchannels,channels, branches=3, reduce=2, stride=1, len=64):
@@ -294,179 +298,152 @@ class AudioClassifierFuse(nn.Module):
         return x_all
 
 
-class AudioClassifier2(nn.Module):
+class AudioClassifierFuseODconv(nn.Module):
     # ----------------------------
     # Build the model architecture
     # ----------------------------
     def __init__(self):
         super().__init__()
-        self.pre = nn.Sequential(nn.ReLU(),
-                                nn.Conv2d(1, 16, kernel_size=7, stride=2, padding=3, bias=False),
-                                nn.BatchNorm2d(16),
-                                # # nn.BatchNorm2d(num_features=8),nn.ReLU(),
-                                nn.ReLU(inplace=True),
-                                # nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-                                )
-        self.SK = SKConv(16, 16)
-        # self.conv1 = nn.Conv2d(8, 32, kernel_size=(3, 3), stride=(1, 1), padding=(2, 2))
+        self.pre = self._pre(1, 16)
+        self.ODconv = ODConv2d(16, 16, 1)
         self.conv1 = nn.Sequential(
-            # nn.Conv2d(16, 16, kernel_size=(3, 3), stride=(1, 1), padding=(2, 2)),
             depthwise_separable_conv(16, 16),
-            # nn.ReLU(),
             nn.BatchNorm2d(16),
             nn.ReLU(),
-            # nn.Dropout(p=0.1),
-            # nn.AvgPool2d(2)
             nn.MaxPool2d(2)
         )
-
-
-        # Second Convolution Block
         self.conv2 = nn.Sequential(
             depthwise_separable_conv(16, 32),
-            # nn.Conv2d(16, 32, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
             nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.MaxPool2d(2)
         )
-        # Third Convolution Block
         self.conv3 = nn.Sequential(
-            # nn.Conv2d(32, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
             depthwise_separable_conv(32, 64),
             nn.BatchNorm2d(64),
             nn.ReLU(),
-            # nn.Dropout(p=0.1),
-            # nn.AvgPool2d(2)
             nn.MaxPool2d(2)
         )
-
-        # Fourth Convolution Block
-        # self.conv4 = nn.Conv2d(64, 128, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
         self.conv4 = nn.Sequential(
-            # nn.Conv2d(64, 128, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
             depthwise_separable_conv(64, 128),
             nn.BatchNorm2d(128),
             nn.ReLU(),
             nn.MaxPool2d(2)
         )
+        self.conv5 = nn.Conv2d(1, 128, kernel_size=1, stride=1, padding=0)
+        # 注意力机制
+        self.channel_att = nn.Sequential(
+            nn.AdaptiveAvgPool2d(1),
+            nn.Conv2d(128, 128, 1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 128, 1, bias=False),
+        )
 
-        # Linear Classifier
         self.ap = nn.AdaptiveAvgPool2d(output_size=1)
+        self.ap2 = nn.AdaptiveAvgPool2d(output_size=(2, 7))
+        self.lin = nn.Linear(in_features=128, out_features=3)
 
 
-        #1Dconv
-        self.conv1_1d =nn.Sequential(
-            nn.Conv1d(1,16,3,1),
-            # depthwise_separable_1Dconv(1,16),
-            nn.BatchNorm1d(16),
-            nn.ReLU(),
-            # nn.Dropout(p=0.1),
-            nn.MaxPool1d(2)
+    def _pre(self, input_channel, outchannel):
+        pre = nn.Sequential(nn.ReLU(),
+            nn.Conv2d(input_channel, outchannel, kernel_size=7, stride=2, padding=3, bias=False),
+            nn.BatchNorm2d(outchannel),
+            nn.ReLU(inplace=True),
         )
-        self.conv2_1d = nn.Sequential(
-            nn.Conv1d(16, 32, 3, 1),
-            # depthwise_separable_1Dconv(16, 32),
-            nn.BatchNorm1d(32),
-            nn.ReLU(),
-            # nn.Dropout(p=0.1),
-            nn.MaxPool1d(2)
-        )
-        self.conv3_1d = nn.Sequential(
-            nn.Conv1d(32, 64, 3, 1),
-            # depthwise_separable_1Dconv(32, 64),
-            nn.BatchNorm1d(64),
-            nn.ReLU(),
-            # nn.Dropout(p=0.1),
-            nn.MaxPool1d(2)
-        )
-        self.conv4_1d = nn.Sequential(
-            nn.Conv1d(64, 128, 3, 1),
-            # depthwise_separable_1Dconv(64, 128),
-            nn.BatchNorm1d(128),
-            nn.ReLU(),
-            # nn.Dropout(p=0.1),
-            nn.MaxPool1d(2)
-        )
-        #batch_first=True时，LSTM接受的数据维度为input(batch_size,特征数，通道数)
-        self.lstm1 = nn.LSTM(128,128,batch_first=True)
-        # self.lstm2 = nn.LSTM(256,128,batch_first=True)
-        self.eca = ECANet(128)
-        self.ap2 =  nn.AdaptiveAvgPool1d(output_size=1)
-
-        self.lin = nn.Linear(in_features=128+128, out_features=3)
-
-
-
+        return pre
     # # ----------------------------
-    # x:一维时域信号，y:二维特征
+    # Forward pass computations
     # ----------------------------
-    def forward(self, x,y):
+    def forward(self, x):
+        # 记录每层的输出
+        # outputs = {}
         # Run the convolutional blocks
-        x = x.unsqueeze(1)
-        x = self.conv1_1d(x)
-        x = self.conv2_1d(x)
-        x = self.conv3_1d(x)
-        x = self.conv4_1d(x)
-        x = x.transpose(-1, -2) #(batch,channel,feat) ->(batch,feat,channel)
-        x, _ = self.lstm1(x)
-        x = x.transpose(-1, -2)
-        x = self.eca(x)
-        x = self.ap2(x)
-        time_feat = x.view(x.shape[0], -1)
+        x = x.unsqueeze(1)  # 为输入特征添加通道，变为(batch_size, 1, height, width)
+        # outputs['input'] = x.shape
+        xf1 = x[:, :, : 64, :]  # 时频域
+        xf2 = x[:, :, 64:, :]  # 时域
+        xf1 = self.pre(xf1)
+        # outputs['pre'] = xf1.shape
+        xf1 = self.ODconv(xf1)
+        # outputs['ODconv'] = xf1.shape
+        xf1 = self.conv1(xf1)
+        # outputs['conv1'] = xf1.shape
+        xf1 = self.conv2(xf1)
+        # outputs['conv2'] = xf1.shape
+        xf1 = self.conv3(xf1)
+        # outputs['conv3'] = xf1.shape
+        xf1 = self.conv4(xf1)
+        # outputs['conv4'] = xf1.shape
+        xf2 = self.ap2(xf2)
+        # outputs['ap2'] = xf2.shape
+        xf2 = self.conv5(xf2)
+        # outputs['conv5'] = xf2.shape
 
-        y = y.unsqueeze(1)
-        y = self.pre(y)
-        y = self.SK(y)
-        y = self.conv1(y)
-        y = self.conv2(y)
-        y = self.conv3(y)
-        y = self.conv4(y)
-        y = self.ap(y)
-        fre_feat = y.view(x.shape[0], -1)
-        all = torch.cat((time_feat, fre_feat), dim=1)
-        out = self.lin(all)
+        # 特征融合
+        x_fuse = xf1 + xf2 + torch.mul(xf1, xf2)
+        # outputs['fuse'] = x_fuse.shape
+
+        # Adaptive pool and flatten for input to linear layer
+        # x_attn = self.ap(x_attn)
+        x_fuse = self.ap(x_fuse)
+        # outputs['ap'] = x_fuse.shape
+        x_all = x_fuse.view(x_fuse.shape[0], -1)
+        # outputs['flatten'] = x_all.shape
+
+        # Linear layer
+        x_all = self.lin(x_all)
+        # outputs['output'] = x_all.shape
+
+        # for layer_name, shape in outputs.items():
+        #     print(f'{layer_name}: {shape}')
 
         # Final output
-        return out
-
-class ECANet(nn.Module):
-    def __init__(self, in_channels, gamma=2, b=1):
-        super(ECANet, self).__init__()
-        self.in_channels = in_channels
-        self.fgp = nn.AdaptiveAvgPool1d(1)
-        kernel_size = int(abs((math.log(self.in_channels, 2) + b) / gamma))
-        kernel_size = kernel_size if kernel_size % 2 else kernel_size + 1
-        self.con1 = nn.Conv1d(1,
-                              1,
-                              kernel_size=kernel_size,
-                              padding=(kernel_size - 1) // 2,
-                              bias=False)
-        self.act1 = nn.Sigmoid()
-
-    def forward(self, x):
-        output = self.fgp(x) #(N,C,W) ->(N,C,1)
-        output = output.transpose(-1, -2) #(N,C,1) ->(N,1,C)
-        output = self.con1(output).transpose(-1, -2) #(N,1,C) ->(N,C,1)
-        output = self.act1(output)
-        output = torch.multiply(x, output)
-        return output
+        return x_all
 
 
 if __name__ == "__main__":
     input1 = torch.rand(10, 12000)
     input2 = torch.rand(10, 64, 239)
-
-    model = AudioClassifier2()
-    # y = torch.rand(128, 8)
-    output = model(input1, input2)
-    total = sum(p.numel() for p in model.parameters())
-    print("Total params: %.2fK" % (total/1e3))
-    input = torch.rand(128,1,12000)
-    model = nn.Conv1d(7,32,3)
-    model = nn.LSTM(256,128,batch_first=True,bidirectional=False)
-    output,_ = model(input)
-    print(output.shape)
     # model = AudioClassifierFuse()
 
-
-
+    b1 = nn.Sequential(nn.ReLU(),
+            nn.Conv2d(1, 16, kernel_size=7, stride=2, padding=3, bias=False),
+            nn.BatchNorm2d(16),
+            nn.ReLU(inplace=True),
+        )
+    # b2 = nn.Sequential(SKConv(16, 16))
+    b2 = nn.Sequential(ODConv2d(16, 16, 3, padding=1))
+    b3 = nn.Sequential(
+            depthwise_separable_conv(16, 16),
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+    b4 = nn.Sequential(
+            depthwise_separable_conv(16, 32),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+    b5 = nn.Sequential(
+            depthwise_separable_conv(32, 64),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+    b6 = nn.Sequential(
+            depthwise_separable_conv(64, 128),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+    net = nn.Sequential(b1, b2, b3, b4, b5, b6, nn.AdaptiveAvgPool2d(output_size=1), nn.Flatten(),
+                        nn.Linear(in_features=128, out_features=3))
+    model = AudioClassifierFuseODconv()
+    X = torch.rand(10, 1, 64, 239)
+    X2 = torch.rand(10, 69, 239)
+    for layer in net:
+        X = layer(X)
+        print(layer.__class__.__name__, 'output shape:\t', X.shape)
+    # output = model(X2)
