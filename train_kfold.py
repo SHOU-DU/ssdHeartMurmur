a8824ff0e88fe27c611161ac652100da12ada629
shou-du <769921010@qq.com>
2024-10-07 19:35:18 +0800

    对所有数据进行分折,训练

--- train_kfold.py
+++ train_kfold.py
@@ -111,8 +111,8 @@ if __name__ == "__main__":
         # 模型选择
         model = AudioClassifierODconv()  # sd Fuse ODconv gamma=2.5
         # model = AudioClassifier()
-        # model_result_path = os.path.join('all_data_TF_MFCC_TDFMVCST_ODC_k3_MM_FCCat133_withoutMFCC', fold_path)
-        model_result_path = os.path.join('all_data_TF_ODConv_k3_weight_2_2_6', fold_path)
+        # model_result_path = os.path.join('all_data_TF_MFCC_TDFMVCST_ODC_k3_FCCat133_withoutMFCC', fold_path)
+        model_result_path = os.path.join('all_data_TF_ODConv_k3_weight_1_1_1', fold_path)
         os.environ['CUDA_VISIBLE_DEVICES'] = '0'
         device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         model = model.to(device)  # 放到设备中
@@ -123,8 +123,8 @@ if __name__ == "__main__":
         # scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, [5, 10, 15, 20, 25, 30], gamma=0.2)  # sd Fuse会过拟合
 
         # 设置损失函数
-        # weight = torch.tensor([1, 1, 1]).to(device)
-        weight = torch.tensor([0.2, 0.2, 0.60]).to(device)  # sd 改变权重值，增加loud权重
+        weight = torch.tensor([1, 1, 1]).to(device)
+        # weight = torch.tensor([0.25, 0.25, 0.50]).to(device)  # sd 改变权重值，增加loud权重
         # criterion = Focal_Loss(gamma=2.5, weight=weight)
         criterion = Focal_Loss(gamma=2.5, weight=weight)  # sd 增大gamma
         # criterion = nn.CrossEntropyLoss()  # sd KAN
