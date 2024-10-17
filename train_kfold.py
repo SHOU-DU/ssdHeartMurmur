caf0d73302c4f2dd6f757e02a6bfcfc839b9bc8f
shou-du <769921010@qq.com>
2024-10-17 17:29:48 +0800

    使用ODC模块模型,focaloss参数1,1,1

--- train_kfold.py
+++ train_kfold.py
@@ -121,8 +121,8 @@ if __name__ == "__main__":
         print("DataLoader is OK")
         # 模型选择
         # model = AudioClassifierFuseODconv()  # sd Fuse ODconv gamma=2.5
-        model = AudioClassifier()
-        CBloss_model_path = r'E:\sdmurmur\ssdHeartMurmur\all_data_results\TF_SK_FocalLoss_1_1_1_old'
+        model = AudioClassifierODconv()
+        CBloss_model_path = r'E:\sdmurmur\ssdHeartMurmur\all_data_results\TF_ODC_FocalLoss_1_1_1_old'
         # model_result_path = os.path.join('all_data_TF_MFCC_TDFMVCST_ODC_k3__FCCat384_25_25_5', fold_path)
         # model_result_path = os.path.join('all_data_TF_ODConv_k3_weight_25_25_5', fold_path)
         model_result_path = os.path.join(CBloss_model_path, fold)
