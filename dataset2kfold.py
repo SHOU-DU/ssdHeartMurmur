09211cac685518701dd5d6870afefba7ca9e42dd
shou-du <769921010@qq.com>
2024-10-21 00:46:11 +0800

    MDN-MARNN模型训练，切分数据,设置重采样率为2500

--- dataset2kfold.py
+++ dataset2kfold.py
@@ -107,7 +107,7 @@ def dataset_split_kfold(data_folder, kfold_folder, kfold=int):
             os.makedirs(os.path.join(kfold_out_dir, "train_data"))
             for index in tqdm(train_idx, desc='calibrated train set cut zero:'):
                 f = pIDs[index]  # 获取patientID
-                cut_copy_files_zero(
+                MDN_MARNN_cut_copy_files_zero(
                     data_folder,
                     f,
                     os.path.join(kfold_out_dir, "train_data/"),
@@ -118,7 +118,7 @@ def dataset_split_kfold(data_folder, kfold_folder, kfold=int):
             os.makedirs(os.path.join(kfold_out_dir, "vali_data"))
             for index in tqdm(val_idx, desc='calibrated vali set cut zero:'):
                 f = pIDs[index]  # 获取patientID
-                cut_copy_files_zero(
+                MDN_MARNN_cut_copy_files_zero(
                     data_folder,
                     f,
                     os.path.join(kfold_out_dir, "vali_data/"),
@@ -213,7 +213,7 @@ def cut_copy_files(data_directory: str, patient_id: str, out_directory: str) -> 
                     end = start + 3 * fs
 
 
-def MDN_MARNN_cut_copy_files(data_directory: str, patient_id: str, out_directory: str) -> None:
+def MDN_MARNN_cut_copy_files_zero(data_directory: str, patient_id: str, out_directory: str) -> None:
     files = os.listdir(data_directory)
     zero_start = []
     zero_end = []
@@ -1159,23 +1159,23 @@ def check_tsv(data_directory: str):
 
 if __name__ == '__main__':
 
-    # # 进行数据分折
-    # original_dataset_folder = r"E:\sdmurmur\calibrated_all_data"  # 对全部数据进行分折
-    # kfold_out = r"E:\sdmurmur\all_data_kfold\with_None_zero"  # grade:soft和loud均匀分折。location:对于present个体，只复制murmur存在的.wav文件
-    # dataset_split_kfold(original_dataset_folder, kfold_out, kfold=5)
+    # 进行数据分折
+    original_dataset_folder = r"E:\sdmurmur\calibratedwithZeroStartEndAllData"  # 对全部数据进行分折
+    kfold_out = r"E:\sdmurmur\all_data_kfold\MDN_MARNN_all_data"  # grade:soft和loud均匀分折。location:对于present个体，只复制murmur存在的.wav文件
+    dataset_split_kfold(original_dataset_folder, kfold_out, kfold=5)
 
     # # 对测试集进行切分和s1,s1幅值缩放操作
     # test_data_folder = r"E:\sdmurmur\calibrated_test_data"  # 校正过的测试集路径
     # scaled_test_folder = "test_data_cut_zero"  # 指定幅值缩放后的路径
     # test_dataset_scale(test_data_folder, scaled_test_folder)
 
-    # 检查tsv文件是否有标记错误
-    original_dataset_folder = r"E:\sdmurmur\calibratedwithZeroStartEndAllData"
-    wrong = check_tsv(original_dataset_folder)
-    new_wrong_list = r"E:\sdmurmur\all_data_kfold\all_data_wrong_list6.txt"
-    with open(new_wrong_list, 'w') as file:
-        for item in wrong:
-            file.write(item + '\n')
+    # # 检查tsv文件是否有标记错误
+    # original_dataset_folder = r"E:\sdmurmur\calibratedwithZeroStartEndAllData"
+    # wrong = check_tsv(original_dataset_folder)
+    # new_wrong_list = r"E:\sdmurmur\all_data_kfold\all_data_wrong_list6.txt"
+    # with open(new_wrong_list, 'w') as file:
+    #     for item in wrong:
+    #         file.write(item + '\n')
 
     # 检查cut_copy_files_s1_s2函数是否正常工作
     # patient_id = '14241'
