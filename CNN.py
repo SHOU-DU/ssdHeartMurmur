ecdf8aebea5034319dfd2a937efefcace49782b9
shou-du <769921010@qq.com>
2024-10-21 20:57:41 +0800

    TF+TDF特征拼接模型，FocalLoss[1,1,1],随机种子10,并用新测试集测试

--- CNN.py
+++ CNN.py
@@ -459,7 +459,7 @@ class AudioClassifierFuseODconv(nn.Module):
         x = x.unsqueeze(1)  # 为输入特征添加通道，变为(batch_size, 1, height, width)
         # outputs['input'] = x.shape
         xf1 = x[:, :, :64, :]  # 时频域
-        xf2 = x[:, :, 128:133, :]  # 时域包络
+        xf2 = x[:, :, 64:69, :]  # 时域包络+MV
         # xf2 = x[:, :, 64:128, :]  # MFCC
         # xf3 = x[:, :, 128:, :]  # 时域包络+均值方差
         # outputs['xf1 shape'] = xf1.shape
@@ -687,7 +687,8 @@ def count_parameters(model):
 
 
 if __name__ == "__main__":
-    model = AudioClassifierMMODconv()
+    # model = AudioClassifierMMODconv()
+    model = AudioClassifierFuseODconv()
     # model = AudioClassifierODconv()
     # model = AudioClassifier()
     print(f"The model has {count_parameters(model):,} trainable parameters")
