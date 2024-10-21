b4975b79ad455ce0e464733703bc2aafb4b0ab5a
shou-du <769921010@qq.com>
2024-10-21 15:30:14 +0800

    提取整合的测试集的loggmma+tdf+mv+cst特征

--- test_feature_extraction.py
+++ test_feature_extraction.py
@@ -40,7 +40,8 @@ def save_test_feature(test_folder, test_tdf_folder, test_feature_folder):
     # train_feature = Log_GF_GAF(kfold_folder_train)
     # train_feature = Log_GF_CWT_PCA(kfold_folder_train, test_tdf_folder)
     # train_feature = Log_GF_TDF(test_folder, test_tdf_folder)
-    train_feature = Log_GF(test_folder)  # 提取单时频域特征
+    # train_feature = Log_GF(test_folder)  # 提取单时频域特征
+    train_feature = Log_GF_TDF_MV_CST(test_folder, test_tdf_folder)  # 提取单时频域特征
 
     train_label, train_location, train_id = get_label(test_folder)  # 获取各个3s片段label和听诊区位置和个体ID
     train_index = get_index(test_folder)
@@ -81,6 +82,73 @@ def Log_GF(data_directory):
             # fbank_feat = feature_norm(fbank_feat)
             # fbank_feat = delt_feature(fbank_feat)
             loggamma.append(fbank_feat)
+
+        else:
+            continue
+    return np.array(loggamma)
+
+
+def Log_GF_TDF_MV_CST(data_directory, TDF_directory):  # 提取时频域和时域特征
+    loggamma = list()
+    # 加tqdm可视化特征提取过程
+    for f in tqdm(sorted(os.listdir(data_directory)), desc=str(data_directory) + ' Log_GF, TDF, MV, CST feature 60Hz:'):
+        root, extension = os.path.splitext(f)
+        if extension == '.wav':
+            x, fs = librosa.load(os.path.join(data_directory, f), sr=4000)
+            x = x - np.mean(x)
+            x = x / np.max(np.abs(x))  # 归一化为[-1, 1]
+            # 对音频数据进行分帧
+            frame_length = int(0.025 * fs)  # 帧长
+            hop_length = int(0.0125 * fs)  # 帧移
+            frames = librosa.util.frame(x, frame_length=frame_length, hop_length=hop_length)
+            # 计算每一帧的均值和方差
+            frame_means = np.mean(frames, axis=0)
+            frame_variances = np.var(frames, axis=0)
+            # 将均值和方差转换成1x帧数的二维数组
+            frame_means_2d = frame_means.reshape(1, -1)
+            frame_variances_2d = frame_variances.reshape(1, -1)
+
+            # gfreqs为经过gammatone滤波器后得到的傅里叶变换矩阵
+            gSpec, gfreqs = erb_spectrogram(x,
+                                            fs=fs,
+                                            pre_emph=0,
+                                            pre_emph_coeff=0.97,
+                                            window=SlidingWindow(0.025, 0.0125, "hamming"),
+                                            nfilts=64,
+                                            nfft=512,
+                                            low_freq=25,
+                                            high_freq=2000)
+            fbank_feat = gSpec.T
+            fbank_feat = np.log(fbank_feat)
+            fbank_feat = feature_norm(fbank_feat)
+            # chroma Feature
+            chromagram = librosa.feature.chroma_stft(y=x, sr=fs, hop_length=50, win_length=100)
+            chromagram = chromagram[:, 0:-2]  # 取前239帧
+            # spectral contrast Feature
+            spectral = np.abs(librosa.stft(x, hop_length=50, win_length=100))
+            contrast = librosa.feature.spectral_contrast(S=spectral, sr=fs, hop_length=50, win_length=100, fmin=20)
+            contrast = contrast[:, 0:-2]
+            # tonnetz Feature
+            y = librosa.effects.harmonic(y=x)  # 提取谐波分量
+            tonnetz = librosa.feature.tonnetz(y=y, sr=fs, hop_length=50, chroma=chromagram)
+
+            # 读取对应的.csv文件
+            csv_file = os.path.join(TDF_directory, root + '.csv')
+            if os.path.exists(csv_file):
+                # csv_data = np.genfromtxt(csv_file, delimiter=',', dtype=None, encoding='utf-8')
+                csv_data = np.loadtxt(csv_file, delimiter=',')
+                # num_cols_to_add = fbank_feat.shape[1] - csv_data.shape[1]
+                # csv_data_pad = np.pad(csv_data, ((0, 0), (0, num_cols_to_add)), mode='constant', constant_values=0)
+                csv_data_cut = csv_data[:, :-1]
+                # 拼接.wav文件特征和.csv文件数据
+                combined_feat = np.concatenate((fbank_feat, csv_data_cut, frame_means_2d, frame_variances_2d,
+                                                chromagram, contrast, tonnetz), axis=0)
+                loggamma.append(combined_feat)
+
+            else:
+                print(f"CSV file not found for {f}")
+
+            # loggamma.append(fbank_feat)
 
         else:
             continue
@@ -212,8 +280,8 @@ def feature_norm(feat):
 if __name__ == '__main__':
     # 特征提取
     kfold_festure_in = "test_data_cut_zero_new"  # test set切割好的数据，对于present个体，只复制murmur存在的.wav文件
-    kfold_feature_folder = "test_feature_TF_cut_zero_new"  # 存储每折特征文件夹
-    tdf_feature_folder = r"E:\sdmurmur\testEnvelopeandSE60Hz"  # 时域特征存储文件夹
+    kfold_feature_folder = "test_feature_TF_TDF_CST_cut_zero_new"  # 存储每折特征文件夹
+    tdf_feature_folder = r"E:\sdmurmur\testEnvelopeandSE60Hz_new"  # 时域特征存储文件夹
     cwt_feature_folder = r"E:\sdmurmur\wavelets\data_kfold_cut_zero"  # cwt特征存储文件夹
     save_test_feature(kfold_festure_in, tdf_feature_folder, kfold_feature_folder)
     print('this is feature extraction file')
